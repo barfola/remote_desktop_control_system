e56b99d14e261d872a1b33c5aa4a2aa6d6f484a1
barfola <yehonatanbarfy@gmail.com>
2024-07-28 22:32:44 +0300

    Now the server can see the client screen and move his mouse. There is a lot of work,fix a little bit the mouse coordinate, and make the screenshot retrieve more efficient.

--- client.py
+++ client.py
@@ -2,6 +2,10 @@
 import socket
 from io import BytesIO
 from PIL import ImageGrab
+import pyautogui
+import pickle
+from helper import get_screen_size
+import threading
 
 
 def send_data(socket_connection: socket.socket, data, is_text=False):
@@ -14,7 +18,7 @@ def send_data(socket_connection: socket.socket, data, is_text=False):
     :return None:
     """
 
-    data_length_in_binary = (1+len(data)).to_bytes(4, byteorder='big')
+    data_length_in_binary = (1 + len(data)).to_bytes(4, byteorder='big')
 
     if isinstance(data, str):
         data = data.encode()
@@ -31,7 +35,7 @@ def send_data(socket_connection: socket.socket, data, is_text=False):
         print('> Connection error during data delivering.')
 
     except Exception as error:
-        print(f'> Connection error during data delivering.{error}.')
+        print(f'> Connection error during data delivering.{error}')
 
 
 def receive_data(socket_connection: socket.socket):
@@ -39,7 +43,7 @@ def receive_data(socket_connection: socket.socket):
     This function receive data according protocol.
     :param socket_connection:
     :return decode_data:
-    :rtype str or None:
+    :rtype str:
     """
     data_length_in_decimal = int.from_bytes(socket_connection.recv(4), byteorder='big')
     data = b''
@@ -54,7 +58,7 @@ def receive_data(socket_connection: socket.socket):
             return None
 
         except Exception as error:
-            print(f'> Error occurred when receiving data. {error}.')
+            print(f'> Error occurred when receiving data.{error}')
             return None
 
     if data[0:1] == b't':
@@ -69,19 +73,48 @@ def get_screenshot():
     return screenshot
 
 
-def send_screen_shot(socket_connection: socket.socket):
+def send_screen_shot(client_socket: socket.socket):
     try:
         while True:
             screenshot = get_screenshot()
-            image_buffer = BytesIO()
-            screenshot.save(image_buffer, format='PNG')
-            image_data = image_buffer.getvalue()
-            send_data(socket_connection, image_data, is_text=False)
+            img_buffer = BytesIO()
+            screenshot.save(img_buffer, format='PNG')
+            img_data = img_buffer.getvalue()
+            send_data(client_socket, img_data, is_text=False)
     finally:
-        socket_connection.close()
+        client_socket.close()
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('10.100.102.16', 50000))
+def receive_server_mouse_coordinates(socket_connection: socket.socket):
+    pyautogui.FAILSAFE = False
+    server_screen_size = receive_data(socket_connection)
+    server_screen_width, server_screen_height = pickle.loads(server_screen_size)
+    client_screen_width, client_screen_height = get_screen_size()
+    screens_width_ratio = client_screen_width / server_screen_width
+    screens_height_ratio = client_screen_height / server_screen_height
+    try:
+        while True:
+            server_mouse_coordinates = receive_data(socket_connection)
+            server_x_coordinate, server_y_coordinate = pickle.loads(server_mouse_coordinates)
+            print(f'server coordinates {server_x_coordinate},{server_y_coordinate}')
+            client_x_coordinate = int(server_x_coordinate * screens_width_ratio)
+            client_y_coordinate = int(server_y_coordinate * screens_height_ratio)
+            print(f'The coordinates are {client_x_coordinate},{client_y_coordinate}')
+            pyautogui.moveTo(client_x_coordinate, client_y_coordinate)
+    except KeyboardInterrupt:
+        print(f'{KeyboardInterrupt} while receiving coordinates.')
+    except Exception as error:
+        print(f'{error} while receiving coordinates.')
 
-send_screen_shot(client_socket)
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(('10.100.102.16', 50000))
+#send_screen_shot(my_socket)
+screen_thread = threading.Thread(target=send_screen_shot, args=(my_socket,))
+#receive_server_mouse_coordinates(socket_connection=my_socket)
+mouse_thread = threading.Thread(target=receive_server_mouse_coordinates, args=(my_socket,))
+screen_thread.start()
+mouse_thread.start()
+
+screen_thread.join()
+mouse_thread.join()
--- server.py
+++ server.py
@@ -3,22 +3,45 @@ from PIL import Image
 import socket
 from io import BytesIO
 import numpy as np
-from helper import receive_data, send_data
+from helper import receive_data, send_data, get_screen_size
 import cv2
+import pyautogui
+import pickle
+import time
+import threading
 
 SERVER_BINDING_IP = '0.0.0.0'
 SERVER_PORT = 50000
-BINDING_IP = '0.0.0.0'
+
+
+def send_server_mouse_coordinates(socket_connection: socket.socket):
+    server_screen_width, server_screen_height = get_screen_size()
+    server_screen_size_in_binary = pickle.dumps((server_screen_width, server_screen_height))
+    send_data(socket_connection, server_screen_size_in_binary)
+    try:
+        while True:
+            x_coordinate, y_coordinate = pyautogui.position()
+            print(f'server coordinates are {x_coordinate},{y_coordinate}')
+            coordinates_in_binary = pickle.dumps((x_coordinate, y_coordinate))
+            send_data(socket_connection, coordinates_in_binary)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print(f'{KeyboardInterrupt} while sending coordinates.')
+    except Exception as error:
+        print(f'{error} while sending coordinates.')
 
 
 def display_client_screen(client_socket: socket.socket):
+    screen_width, screen_height = get_screen_size()
+    screen_height = screen_height - 80
     while True:
         image_binary_data = receive_data(client_socket)
         if image_binary_data:
             try:
                 pil_image = Image.open(BytesIO(image_binary_data))
                 cv2_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
-                cv2.imshow('CONTROLLED PC SCREEN', cv2_image)
+                cv2_image_in_screen_size = cv2.resize(cv2_image, (screen_width, screen_height))
+                cv2.imshow('CONTROLLED PC SCREEN', cv2_image_in_screen_size)
             except OSError:
                 print(f'Invalid image.{OSError}.')
             except Exception as error:
@@ -29,8 +52,15 @@ def display_client_screen(client_socket: socket.socket):
 
 
 def handle_client(client_socket: socket.socket):
-    display_client_screen(client_socket)
+    mouse_thread = threading.Thread(target=send_server_mouse_coordinates, args=(client_socket,))
+    screen_thread = threading.Thread(target=display_client_screen, args=(client_socket,))
 
+    #display_client_screen(client_socket)
+    #send_server_mouse_coordinates(client_socket)
+    mouse_thread.start()
+    screen_thread.start()
+    mouse_thread.join()
+    screen_thread.join()
 
 def running_server_on_tcp_socket(server_binding_ip, port_number, number_of_listeners=1):
     """
@@ -57,3 +87,7 @@ def running_server_on_tcp_socket(server_binding_ip, port_number, number_of_liste
 
 running_server_on_tcp_socket(SERVER_BINDING_IP, SERVER_PORT)
 
+#Error with displaying the client screen.only integer scalar arrays can be converted to a scalar index.
+# if __name__ == '__main__':
+#     while True:
+#         print(pyautogui.position())
